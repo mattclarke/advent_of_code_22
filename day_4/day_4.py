13366481cfec85650454dcdca64974b7f0d12c97
Matt Clarke <matt.clarke@esss.se>
2022-12-03 09:44:17 +0100

    Prep for Day 4

---
+++ day_4/day_4.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    PUZZLE_INPUT = f.read()
+
+# PUZZLE_INPUT = """
+# """
+
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
+
+result = 0
+
+for l in lines:
+    pass
+
+# Part 1 = 
+print(f"answer = {result}")
+
+# Part 2 = 
+print(f"answer = {result}")
