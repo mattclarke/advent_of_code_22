1773898bfaf5bc7f9410b8f82e8acab906d78169
Matt Clarke <matt.clarke@esss.se>
2022-12-04 11:28:16 +0100

    use args to get file

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,5 +1,11 @@
-with open("input.txt") as f:
-    PUZZLE_INPUT = f.read()
+import sys
+
+
+FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+print(FILE)
+
+with open(FILE) as f:
+    PUZZLE_INPUT = f.read().strip()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
 print(lines)
@@ -9,7 +15,7 @@ result = 0
 for l in lines:
     pass
 
-# Part 1 = 
+# Part 1 =
 print(f"answer = {result}")
 
 result = 0
@@ -17,5 +23,5 @@ result = 0
 for l in lines:
     pass
 
-# Part 2 = 
+# Part 2 =
 print(f"answer = {result}")
