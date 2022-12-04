a44846f9d300bad8734502e4f651a1e3533b87c3
Matt Clarke <matt.clarke@esss.se>
2022-12-04 06:18:01 +0100

    day 4 done

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Initially I did it "long hand" but went back and refactored to make it more comp
 - Part 1: loop, split and use a set to find the item that appears in both.
 - Part 2: similar but compare three elves to find the common item.
 
+## Day 4
+- Part 1: convert to ints and see if the whole range is contained.
+- Part 2: similar but see if either of the ends of one range are enclosed by the other range.
+
--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,18 +1,40 @@
 with open("input.txt") as f:
     PUZZLE_INPUT = f.read()
 
-# PUZZLE_INPUT = """
-# """
-
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
 
 result = 0
 
 for l in lines:
-    pass
+    f, s = l.split(",")
+    f1, f2 = f.split("-")
+    f1, f2 = int(f1), int(f2)
+    s1, s2 = s.split("-")
+    s1, s2 = int(s1), int(s2)
+    if f1 <= s1 and f2 >= s2:
+        result += 1
+    elif s1 <= f1 and s2 >= f2:
+        result += 1
 
-# Part 1 = 
+# Part 1 = 518
 print(f"answer = {result}")
 
-# Part 2 = 
+result = 0
+
+for l in lines:
+    f, s = l.split(",")
+    f1, f2 = f.split("-")
+    f1, f2 = int(f1), int(f2)
+    s1, s2 = s.split("-")
+    s1, s2 = int(s1), int(s2)
+    if f1 <= s1 and f2 >= s1:
+        result += 1
+    elif f1 <= s2 and f2 >= s2:
+        result += 1
+    elif s1 <= f1 and s2 >= f1:
+        result += 1
+    elif s1 <= f2 and s2 >= f2:
+        result += 1
+
+# Part 2 = 909
 print(f"answer = {result}")
