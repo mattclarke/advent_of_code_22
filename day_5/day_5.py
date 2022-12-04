e65ddcde3e7c6671d6a669b5958b3a047dfa20a9
Matt Clarke <matt.clarke@esss.se>
2022-12-04 06:22:37 +0100

    added day 5 skeleton

---
+++ day_5/day_5.py
@@ -0,0 +1,21 @@
+with open("input.txt") as f:
+    PUZZLE_INPUT = f.read()
+
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
+print(lines)
+
+result = 0
+
+for l in lines:
+    pass
+
+# Part 1 = 
+print(f"answer = {result}")
+
+result = 0
+
+for l in lines:
+    pass
+
+# Part 2 = 
+print(f"answer = {result}")
