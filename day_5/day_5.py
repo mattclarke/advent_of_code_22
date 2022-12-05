07a2998782f63424b2f39551ad93198b5fc648f4
Matt Clarke <matt.clarke@esss.se>
2022-12-05 06:51:20 +0100

    day 5 done

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,27 +1,71 @@
+import copy
 import sys
 
 
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-print(FILE)
 
 with open(FILE) as f:
-    PUZZLE_INPUT = f.read().strip()
+    PUZZLE_INPUT = f.read().rstrip()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+lines = [line for line in PUZZLE_INPUT.split("\n") if line]
 
-result = 0
+num_stack = (len(lines[0]) // 4) + 1
+
+stacks = { n + 1: [] for n in range(num_stack)}
+moves = []
 
 for l in lines:
-    pass
+    if '[' in l:
+        cons = []
+        cnt = 0
+        foo = ""
+        for i, ch in enumerate(l):
+            if cnt == 3:
+                cons.append(foo)
+                cnt = 0
+                foo = ''
+                continue
+            foo += ch
+            cnt += 1
+        cons.append(foo)
 
-# Part 1 =
+        i = 1
+        for c in cons:
+            if c.strip():
+                stacks[i].insert(0, c[1])
+            i += 1
+    if l.startswith('move'):
+        v = l.replace('move ', '').replace('from ', '').replace('to ', '')
+        nums = [int(x) for x in v.split(' ')]
+        moves.append(nums)
+
+stacks_1 = copy.deepcopy(stacks)
+
+for mo in moves:
+    num, fro, to = mo
+    for i in range(num):
+        o = stacks_1[fro].pop(~0)
+        stacks_1[to].append(o)
+
+result = ""
+for n,v in stacks_1.items():
+    result += v[~0]
+
+# Part 1 = LBLVVTVLP
 print(f"answer = {result}")
 
-result = 0
+for mo in moves:
+    num, fro, to = mo
+    st = []
+    for i in range(num):
+        st.append(stacks[fro].pop(~0))
+    while st:
+        o = st.pop(~0)
+        stacks[to].append(o)
 
-for l in lines:
-    pass
+result = ""
+for n,v in stacks.items():
+    result += v[~0]
 
-# Part 2 =
+# Part 2 = TPFFBDRJD
 print(f"answer = {result}")
