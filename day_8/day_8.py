74a833bbb6b976578b4cd2be5db23144d34498b9
Matt Clarke <matt.clarke@esss.se>
2022-12-08 07:58:06 +0100

    day 8 done

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -9,18 +9,91 @@ with open(FILE) as f:
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
 
-result = 0
+TREES = []
+SEEN = set()
 
 for l in lines:
-    pass
+    row = []
+    for c in l:
+        row.append(int(c))
+    TREES.append(row)
 
-# Part 1 = 
+cols = [-1 for _ in range(len(TREES[0]))]
+    
+for r, row in enumerate(TREES):
+    height = -1
+    for c, t in enumerate(row):
+        if t > height:
+            height = t
+            SEEN.add((r,c))
+    height =-1
+    for c, t in enumerate(reversed(row)):
+        if t > height:
+            height = t
+            SEEN.add((r,len(cols) - c - 1))
+    for c, t in enumerate(row):
+        if t> cols[c]:
+            cols[c] = t
+            SEEN.add((r,c))
+
+cols = [-1 for _ in range(len(TREES[0]))]
+for r, row in enumerate(reversed(TREES)):
+    for c, t in enumerate(row):
+        if t> cols[c]:
+            cols[c] = t
+            SEEN.add((len(TREES) - r - 1,c))
+
+result = len(SEEN)
+
+# Part 1 = 1647
 print(f"answer = {result}")
 
 result = 0
 
-for l in lines:
-    pass
+for s in SEEN:
+    r = s[0] - 1
+    rmax = TREES[s[0]][s[1]]
+    total = 1
 
-# Part 2 = 
+    score = 0
+    while r >= 0:
+        if TREES[r][s[1]] >= rmax:
+            score += 1
+            break
+        score += 1
+        r -= 1
+    total *= score
+
+    score = 0
+    r = s[0] + 1
+    while r < len(TREES):
+        if TREES[r][s[1]] >= rmax:
+            score += 1
+            break
+        score += 1
+        r += 1
+    total *= score
+
+    score = 0
+    c = s[1] - 1 
+    while c >= 0:
+        if TREES[s[0]][c] >= rmax:
+            score += 1
+            break
+        score += 1
+        c -= 1
+    total *= score
+
+    score = 0
+    c = s[1] + 1 
+    while c < len(TREES):
+        if TREES[s[0]][c] >= rmax:
+            score += 1
+            break
+        score += 1
+        c += 1
+    total *= score
+    result = max(total, result)
+
+# Part 2 = 392080 
 print(f"answer = {result}")
