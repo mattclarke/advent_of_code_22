3fcc7f0602ed5595937c89b695d44eb2f191d915
Matt Clarke <matt.clarke@esss.se>
2022-12-09 07:36:13 +0100

    refactoring

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -15,72 +15,57 @@ for l in lines:
     d, n = l.split()
     MOVES.append((d, int(n)))
 
+
+def move_head(h_pos):
+    if d == "U":
+        h_pos = (h_pos[0], h_pos[1] + 1)
+    elif d == "D":
+        h_pos = (h_pos[0], h_pos[1] - 1)
+    elif d == "R":
+        h_pos = (h_pos[0] + 1, h_pos[1])
+    elif d == "L":
+        h_pos = (h_pos[0] - 1, h_pos[1])
+    return h_pos
+
+
+def follow(h_pos, t_pos):
+    # If adjacent then don't move tail
+    if abs(h_pos[0] - t_pos[0]) <= 1 and abs(h_pos[1] - t_pos[1]) <= 1:
+        return t_pos
+    if t_pos[0] < h_pos[0]:
+        t_pos = (t_pos[0] + 1, t_pos[1])
+    elif t_pos[0] > h_pos[0]:
+        t_pos = (t_pos[0] - 1, t_pos[1])
+    if t_pos[1] < h_pos[1]:
+        t_pos = (t_pos[0], t_pos[1] + 1)
+    elif t_pos[1] > h_pos[1]:
+        t_pos = (t_pos[0], t_pos[1] - 1)
+    return t_pos
+
+
 SEEN = set()
 h_pos = (0, 0)
 t_pos = (0, 0)
-SEEN.add(t_pos)
 
 for mv in MOVES:
-    d, n = mv
-    for _ in range(n):
-        if d == "U":
-            h_pos = (h_pos[0], h_pos[1] + 1)
-        elif d == "D":
-            h_pos = (h_pos[0], h_pos[1] - 1)
-        elif d == "R":
-            h_pos = (h_pos[0] + 1, h_pos[1])
-        elif d == "L":
-            h_pos = (h_pos[0] - 1, h_pos[1])
-
-        # If adjecent then don't move tail
-        if abs(h_pos[0] - t_pos[0]) <= 1 and abs(h_pos[1] - t_pos[1]) <= 1:
-            continue
-
-        if t_pos[0] < h_pos[0]:
-            t_pos = (t_pos[0] + 1, t_pos[1])
-        elif t_pos[0] > h_pos[0]:
-            t_pos = (t_pos[0] - 1, t_pos[1])
-        if t_pos[1] < h_pos[1]:
-            t_pos = (t_pos[0], t_pos[1] + 1)
-        elif t_pos[1] > h_pos[1]:
-            t_pos = (t_pos[0], t_pos[1] - 1)
+    d, amt = mv
+    for _ in range(amt):
+        h_pos = move_head(h_pos)
+        t_pos = follow(h_pos, t_pos)
         SEEN.add(t_pos)
 
 # Part 1 = 6030
 print(f"answer = {len(SEEN)}")
 
 SEEN = set()
-SEEN.add((0, 0))
 knots = [(0, 0) for _ in range(10)]
 
 for mv in MOVES:
-    d, n = mv
-    for _ in range(n):
-        h_pos = knots[0]
-        if d == "U":
-            h_pos = (h_pos[0], h_pos[1] + 1)
-        elif d == "D":
-            h_pos = (h_pos[0], h_pos[1] - 1)
-        elif d == "R":
-            h_pos = (h_pos[0] + 1, h_pos[1])
-        elif d == "L":
-            h_pos = (h_pos[0] - 1, h_pos[1])
-        knots[0] = h_pos
+    d, amt = mv
+    for _ in range(amt):
+        knots[0] = move_head(knots[0])
         for k in range(1, len(knots)):
-            h_pos = knots[k - 1]
-            t_pos = knots[k]
-            if abs(h_pos[0] - t_pos[0]) <= 1 and abs(h_pos[1] - t_pos[1]) <= 1:
-                continue
-
-            if t_pos[0] < h_pos[0]:
-                t_pos = (t_pos[0] + 1, t_pos[1])
-            elif t_pos[0] > h_pos[0]:
-                t_pos = (t_pos[0] - 1, t_pos[1])
-            if t_pos[1] < h_pos[1]:
-                t_pos = (t_pos[0], t_pos[1] + 1)
-            elif t_pos[1] > h_pos[1]:
-                t_pos = (t_pos[0], t_pos[1] - 1)
-            knots[k] = t_pos
+            knots[k] = follow(knots[k - 1], knots[k])
         SEEN.add(knots[~0])
 
 # Part 2 = 2545
