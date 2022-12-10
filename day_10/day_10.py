2c261728eb66330c93ea6f1e4e712095110354d0
Matt Clarke <matt.clarke@esss.se>
2022-12-10 07:13:28 +0100

    part 1 done

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -8,14 +8,33 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
 result = 0
 
-for l in lines:
-    pass
+X =1
+Q = None
+cycle = 1
+finish = False
+busy = False
+add = 0
 
-# Part 1 = 
+while lines or Q is not None:
+    if cycle in  [20, 60, 100, 140, 180, 220]:
+        result += cycle * X
+    if lines and Q is None:
+        l = lines.pop(0)
+        if l != 'noop':
+            op, val = l.split(' ')
+            Q = int(val)
+            cycle += 1
+            continue
+    if Q is not None:
+        X += Q
+        Q = None
+    cycle += 1
+    
+
+# Part 1 = 13860 
 print(f"answer = {result}")
 
 result = 0
