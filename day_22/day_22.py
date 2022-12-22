d8122d021e98e1a7d4663bd64d7469c598f3edc7
Matt Clarke <matt.clarke@esss.se>
2022-12-22 14:27:46 +0100

    messy but passes pt 2 example

--- day_22/day_22.py
+++ day_22/day_22.py
@@ -14,7 +14,8 @@ START = None
 PUZZLE = ''
 max_ = 0
 for r, l in enumerate(lines):
-    max_ = max(max_, len(l))
+    if l[0] in ['.', ' ', '#']:
+        max_ = max(max_, len(l))
 
 for r, l in enumerate(lines):
     if l:
@@ -134,15 +135,172 @@ while puzzle:
         heading = (heading + 1) % 4
     elif rot == 'L':
         heading = (heading - 1) % 4
-    #show(GRID, pos)
 
-print(pos, heading)
 result = 1000*(pos[0] + 1) + 4*(pos[1] + 1) + heading
 
 # Part 1 = 109094
 print(f"answer = {result}")
 
-result = 0
+FACES = {1: [], 2:[], 3:[], 4: [], 5: [], 6:[]}
+FACE_COORDS = {}
+
+size, width, height = (len(GRID) // 4, 3, 4) if len(GRID) > len(GRID[0]) else (len(GRID[0]) //4, 4, 3)
+
+face = 1
+for h in range(height):
+    for w in range(width):
+        if  GRID[size*h][size*w] == ' ':
+            continue
+        FACE_COORDS[face] = (size * h, size * w)
+        for r in range(size*h, size*h + size):
+            row = []
+            for c in range(size*w, size*w + size):
+                row.append(GRID[r][c])
+            FACES[face].append(row)
+        face += 1
+
+RIGHT = 0
+DOWN = 1
+LEFT = 2
+UP = 3
+
+JOINS = {
+    # right, down, left, up
+    1: ((6, lambda r, c: (size - r-1, size - 1), LEFT), (4, lambda r,c: (0, c), DOWN), (3, lambda r, c: (0, r), DOWN), (2, lambda r,c: (0, size - c-1), DOWN)),
+    2: ((3, lambda r, c: (r, 0), RIGHT), (5, lambda r,c: (size - 1, size - c-1), UP), (6, lambda r, c: (size - 1, size - r - 1), UP), (1, lambda r,c: (0, size - c-1), DOWN)),
+    3: ((4, lambda r, c: (r, 0), RIGHT), (5, lambda r,c: (size - c-1, RIGHT), 0), (2, lambda r, c: (r, size - 1), LEFT), (1, lambda r,c: (c, 0), RIGHT)),
+    4: ((6, lambda r, c: (0, size-r-1), DOWN), (5, lambda r,c: (0, c), DOWN), (3, lambda r, c: (r, size - 1), LEFT), (1, lambda r,c: (size-1, c), UP)),
+    5: ((6, lambda r, c: (r, 0), RIGHT), (2, lambda r,c: (size-1, size - c-1), UP), (3, lambda r, c: (size - 1, size - r -1), UP), (4, lambda r,c: (size-1, c), UP)),
+    6: ((1, lambda r, c: (size - r-1, size - 1), LEFT), (2, lambda r,c: (size -c - 1, 0), RIGHT), (5, lambda r, c: (r, size - 1), LEFT), (4, lambda r,c: (size-c-1, size-1), LEFT)),
+}
+
+if FILE == "input.txt":
+    assert False
+
+
+def print_face(nface, pos):
+    face = FACES[nface]
+    print("face", nface)
+    for r, row in enumerate(face):
+        tmp = []
+        for c, ch in enumerate(row):
+            if (r,c) == pos:
+                tmp.append("P")
+            else:
+                tmp.append(ch)
+        print("".join(tmp))
+    print()
+
+puzzle = copy.copy(PUZZLE)
+heading = 0
+curr_face = 1
+pos = (0, 0)
+
+#while True:
+#    d = input(">>> ")
+#    if d in ['r', 'l', 'u', 'd']:
+#        if d == 'r':
+#            npos = (pos[0], pos[1]+1)
+#            if npos[1] >= size:
+#                f, conv, nd = JOINS[curr_face][0]
+#                npos = conv(*pos)
+#                curr_face = f
+#        elif d == 'd':
+#            npos = (pos[0]+1, pos[1])
+#            if npos[0] >= size:
+#                f, conv, nd = JOINS[curr_face][1]
+#                npos = conv(*pos)
+#                curr_face = f
+#        elif d == 'l':
+#            npos = (pos[0], pos[1]-1)
+#            if npos[1] < 0:
+#                f, conv, nd = JOINS[curr_face][2]
+#                npos = conv(*pos)
+#                curr_face = f
+#        elif d == 'u':
+#            npos = (pos[0]-1, pos[1])
+#            if npos[0] < 0:
+#                f, conv, nd = JOINS[curr_face][3]
+#                npos = conv(*pos)
+#                curr_face = f
+#        pos = npos
+#        print_face(curr_face, pos)
+#    elif d == 'q':
+#        break
+#    else:
+#        print("ignore")
+
+print_face(curr_face, pos)
+
+while puzzle:
+    dist = puzzle.pop(0)
+    rot = puzzle.pop(0) if puzzle else ''
+    print(pos, curr_face, heading, dist, rot)
+    for _ in range(dist):
+        if heading == RIGHT:
+            npos = (pos[0], pos[1]+1)
+            try:
+                if FACES[curr_face][npos[0]][npos[1]] == "#":
+                    break
+            except:
+                pass
+            if npos[1] >= size:
+                f, conv, nd = JOINS[curr_face][0]
+                npos = conv(*pos)
+                if FACES[f][npos[0]][npos[1]] == "#":
+                    break
+                curr_face = f
+                heading = nd
+        elif heading == DOWN:
+            npos = (pos[0]+1, pos[1])
+            try:
+                if FACES[curr_face][npos[0]][npos[1]] == "#":
+                    break
+            except:
+                pass
+            if npos[0] >= size:
+                f, conv, nd = JOINS[curr_face][1]
+                npos = conv(*pos)
+                if FACES[f][npos[0]][npos[1]] == "#":
+                    break
+                curr_face = f
+                heading = nd
+        elif heading == LEFT:
+            npos = (pos[0], pos[1]-1)
+            try:
+                if FACES[curr_face][npos[0]][npos[1]] == "#":
+                    break
+            except:
+                pass
+            if npos[1] < 0:
+                f, conv, nd = JOINS[curr_face][2]
+                npos = conv(*pos)
+                if FACES[f][npos[0]][npos[1]] == "#":
+                    break
+                curr_face = f
+                heading = nd
+        elif heading == UP:
+            npos = (pos[0]-1, pos[1])
+            try:
+                if FACES[curr_face][npos[0]][npos[1]] == "#":
+                    break
+            except:
+                pass
+            if npos[0] < 0:
+                f, conv, nd = JOINS[curr_face][3]
+                npos = conv(*pos)
+                if FACES[f][npos[0]][npos[1]] == "#":
+                    break
+                curr_face = f
+                heading = nd
+        pos = npos
+        print_face(curr_face, pos)
+    if rot == 'R':
+        heading = (heading + 1) % 4
+    elif rot == 'L':
+        heading = (heading - 1) % 4
+
+result = 1000*(FACE_COORDS[curr_face][0] + pos[0] + 1) + 4*(FACE_COORDS[curr_face][1]+pos[1] + 1) + heading
 
 # Part 2 = 
 print(f"answer = {result}")
