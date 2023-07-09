545baf6cefa83106a4b25d04f8fa5f1da7f810a6
Matt Clarke <matt.clarke@ess.eu>
2023-07-09 15:48:38 +0200

    9: update part 1 to use part 2

--- day_9/solver.exs
+++ day_9/solver.exs
@@ -10,14 +10,9 @@ input_data =
 
 defmodule Foo do
   def solve1(input_data) do
-    head_pos = {0, 0}
-    tail_pos = {0, 0}
+    knots = [{0, 0}, {0, 0}]
 
-    visited =
-      MapSet.new()
-      |> MapSet.put(tail_pos)
-
-    move_head(input_data, head_pos, tail_pos, visited)
+    do_instruction(input_data, knots, MapSet.new())
   end
 
   def solve2(input_data) do
@@ -29,7 +24,7 @@ defmodule Foo do
   end
 
   defp do_instruction([{direction, steps} | tail], knots, visited) do
-    {new_knots, new_visited} = do_steps(direction, steps, knots, visited)
+    {new_knots, new_visited} = move_head(direction, steps, knots, visited)
     do_instruction(tail, new_knots, new_visited)
   end
 
@@ -37,51 +32,31 @@ defmodule Foo do
     visited
   end
 
-  defp do_steps(direction, steps, [head | tail], visited) do
+  defp move_head(direction, steps, [head | tail], visited) do
     new_head = step_head(head, direction)
-    new_tail = applesauce([new_head | tail])
+    new_tail = update_followers([new_head | tail])
     knots = [new_head | new_tail]
     visited = MapSet.put(visited, List.last(knots))
     if steps == 1 do
       {knots, visited}
     else
-      do_steps(direction, steps - 1, knots, visited)
+      move_head(direction, steps - 1, knots, visited)
     end
   end
 
-  defp applesauce([first | tail]) do
+  defp update_followers([first | tail]) do
     if length(tail) > 0 do
       [second | tail] = tail
       second = step_tail(first, second)
-      result = applesauce([second | tail])
+      result = update_followers([second | tail])
       [second | result]
     else
       []
     end
   end
 
-  defp applesauce([]) do
+  defp update_followers([]) do
     []
-  end
-
-  defp move_head([{direction, steps} | tail], head_pos, tail_pos, visited) do
-    {new_head_pos, new_tail_pos} = move_pair(direction, head_pos, tail_pos)
-    new_visited = MapSet.put(visited, new_tail_pos)
-
-    if steps == 1 do
-      move_head(tail, new_head_pos, new_tail_pos, new_visited)
-    else
-      move_head([{direction, steps - 1} | tail], new_head_pos, new_tail_pos, new_visited)
-    end
-  end
-
-  defp move_head([], _head_pos, _tail_pos, visited) do
-    visited
-  end
-
-  defp move_pair(direction, head_pos, tail_pos) do
-    new_head_pos = step_head(head_pos, direction)
-    {new_head_pos, step_tail(new_head_pos, tail_pos)}
   end
 
   defp step_head(head_pos, direction) do
