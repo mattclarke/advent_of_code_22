d30ca336d1f29fd7a80af7e081ea98225d545189
Matt Clarke <matt.clarke@ess.eu>
2023-07-12 09:21:59 +0200

    10: part 2

--- day_10/solver.exs
+++ day_10/solver.exs
@@ -15,21 +15,21 @@ input_data =
 
 defmodule Foo do
   def solve(input_data) do
-    state = %{cycle: 1, x: 1, result: %{}}
-    Enum.reduce(input_data, state, &applesauce/2)
+    state = %{cycle: 1, x: 1, result: %{1 => 1}}
+    Enum.reduce(input_data, state, &run_command/2)
   end
 
-  def applesauce({"addx", num}, %{cycle: cycle, x: x, result: result}) do
+  def run_command({"addx", num}, %{cycle: cycle, x: x, result: result}) do
     result = Map.put(result, cycle + 1, x)
     %{cycle: cycle + 2, x: x + num, result: Map.put(result, cycle + 2, x + num)}
   end
 
-  def applesauce({"noop"}, %{cycle: cycle, x: x, result: result}) do
+  def run_command({"noop"}, %{cycle: cycle, x: x, result: result}) do
     %{cycle: cycle + 1, x: x, result: Map.put(result, cycle + 1, x)}
   end
 end
 
-%{result: result} = Foo.solve(input_data)
+%{cycle: cycles, result: result} = Foo.solve(input_data)
 
 total =
   [20, 60, 100, 140, 180, 220]
@@ -39,5 +39,15 @@ total =
 
 IO.puts("Answer to part 1 = #{total}")
 
-# {_, result} = Foo.solve(input_data, 10)
-# IO.puts("Answer to part 2 = #{MapSet.size(result)}")
+1..cycles - 1
+|> Enum.map(fn y ->
+  x = Map.get(result, y)
+  if x - 1 <= rem(y - 1, 40) and rem(y - 1, 40) <= x + 1 do
+    IO.write("#")
+  else
+    IO.write(" ")
+  end
+  if rem(y, 40) == 0 do
+    IO.puts("")
+  end
+end)
