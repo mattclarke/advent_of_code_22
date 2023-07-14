8888a533ba6bc4468f31f0f04a98dbfde550152c
Matt Clarke <matt.clarke@ess.eu>
2023-07-14 08:24:32 +0200

    11: part 2 done, didn't read the instructions properly

--- day_11/solver.exs
+++ day_11/solver.exs
@@ -21,36 +21,37 @@ rules = %{
 }
 
 defmodule Foo do
-  def solve(monkeys, rules, number_rounds) do
+  def solve(monkeys, rules, number_rounds, worry_adjuster) do
     1..number_rounds
     |> Enum.reduce(monkeys, fn _, acc ->
-      perform_round(acc, rules)
+      perform_round(acc, rules, worry_adjuster)
     end)
   end
 
-  defp perform_round(monkeys, rules) do
-    inspect_items(monkeys, rules, 0)
+  defp perform_round(monkeys, rules, worry_adjuster) do
+    inspect_items(monkeys, rules, 0, worry_adjuster)
   end
 
-  defp inspect_items(monkeys, _rules, 8) do
+  defp inspect_items(monkeys, _rules, 8, _worry_adjuster) do
     monkeys
   end
 
-  defp inspect_items(monkeys, rules, current) do
+  defp inspect_items(monkeys, rules, current, worry_adjuster) do
     {items, inspections} = Map.get(monkeys, current)
     r = Map.get(rules, current)
     new_monkeys =
       Enum.reduce(items, monkeys, fn x, acc ->
-        {dest, worry} = inspect_item(x, r)
+        {dest, worry} = inspect_item(x, r, worry_adjuster)
         {ditems, dinspections} = Map.get(acc, dest)
         %{acc | dest => {ditems ++ [worry], dinspections}}
       end)
     new_monkeys = %{new_monkeys | current => {[], inspections + length(items)}}
-    inspect_items(new_monkeys, rules, current + 1)
+    inspect_items(new_monkeys, rules, current + 1, worry_adjuster)
   end
 
-  defp inspect_item(item, rules) do
-    new_worry = Kernel.trunc(rules.func.(item) / 3)
+  defp inspect_item(item, rules, worry_adjuster) do
+    new_worry = Kernel.trunc(rules.func.(item))
+    new_worry = worry_adjuster.(new_worry)
     case rem(new_worry, rules.test) do
       0 -> {rules.true, new_worry}
       _ -> {rules.false, new_worry}
@@ -59,22 +60,26 @@ defmodule Foo do
 end
 
 result =
-  Foo.solve(monkeys, rules, 20)
+  Foo.solve(monkeys, rules, 20, fn x -> Kernel.trunc(x / 3) end)
   |> Map.values()
   |> Stream.map(fn {_, v} -> v end)
   |> Enum.sort(:desc)
-  |> IO.inspect()
   |> Enum.take(2)
   |> Enum.reduce(1, fn x, acc -> x * acc end)
 
 IO.puts("Answer to part 1 = #{result}")
 
+lcm =
+  rules
+  |> Map.values()
+  |> Stream.map(fn v -> v.test end)
+  |> Enum.reduce(1, fn x, acc -> x * acc end)
+
 result =
-  Foo.solve(monkeys, rules, 10000)
+  Foo.solve(monkeys, rules, 10000, fn x -> rem(x, lcm) end)
   |> Map.values()
   |> Stream.map(fn {_, v} -> v end)
   |> Enum.sort(:desc)
-  |> IO.inspect()
   |> Enum.take(2)
   |> Enum.reduce(1, fn x, acc -> x * acc end)
 
