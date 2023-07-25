2a7aa90010c8b8e0cd02a7269c00ea791cf77342
Matt Clarke <matt.clarke@ess.eu>
2023-07-25 10:41:04 +0200

    15: part 2 done

--- day_15/solver.exs
+++ day_15/solver.exs
@@ -15,7 +15,7 @@ input_data =
   end)
 
 defmodule Foo do
-  def solve(input_data, target_row) do
+  def solve1(input_data, target_row) do
     occupied =
       input_data
       |> Enum.reduce(MapSet.new(), fn [sensor, beacon], acc ->
@@ -26,6 +26,45 @@ defmodule Foo do
     |> Enum.reduce(MapSet.new(), fn [sensor, beacon], acc ->
       examine_sensor(sensor, beacon, target_row, occupied, acc)
     end)
+  end
+
+  def solve2(input_data, size) do
+    sensors_to_manhatten =
+      Enum.reduce(input_data, %{}, fn [sensor, beacon], acc ->
+        Map.put(acc, sensor, calculate_manhatten_distance(sensor, beacon))
+      end)
+
+    check_row(0, 0, sensors_to_manhatten, size)
+  end
+
+  defp check_row(x, y, sensors, size) when x >= size do
+    check_row(0, y + 1, sensors, size)
+  end
+
+  defp check_row(x, y, sensors, size) do
+    new_step =
+      Enum.reduce(sensors, 0, fn sensor, acc ->
+        {{sx, sy}, manhatten_distance} = sensor
+
+        if in_range(x, y, {sx, sy}, manhatten_distance) do
+          max(sx + (manhatten_distance - abs(y - sy) + 1) - x, acc)
+        else
+          acc
+        end
+      end)
+
+    if new_step == 0 do
+      {x + new_step, y}
+    else
+      check_row(x + new_step, y, sensors, size)
+    end
+  end
+
+  defp in_range(x, y, {sx, sy}, manhatten_distance) do
+    cond do
+      abs(x - sx) + abs(y - sy) > manhatten_distance -> false
+      true -> true
+    end
   end
 
   defp examine_sensor(sensor = {sx, sy}, beacon, target_row, occupied, not_empty) do
@@ -53,6 +92,11 @@ defmodule Foo do
   end
 end
 
-result = MapSet.size(Foo.solve(input_data, 2_000_000))
+result = MapSet.size(Foo.solve1(input_data, 2_000_000))
 
 IO.puts("Answer to part 1 = #{result}")
+
+{x, y} = Foo.solve2(input_data, 4_000_000)
+result = 4_000_000 * x + y
+
+IO.puts("Answer to part 2 = #{result}")
