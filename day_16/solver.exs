1922f6e85d425f216ae6cd933f49d7a1c4c49b9d
Matt Clarke <matt.clarke@ess.eu>
2023-07-26 11:41:26 +0200

    16: part 1 done but is slow

--- day_16/solver.exs
+++ day_16/solver.exs
@@ -3,19 +3,57 @@ file = "input.txt"
 input_data =
   File.stream!(file)
   |> Stream.map(&String.replace(&1, "\n", ""))
-  |> Enum.map(fn x ->
+  |> Enum.reduce(%{}, fn x, acc ->
     [valve, flow | tunnels] =
       x
       |> String.split([" ", "=", ";", ", "])
       |> Enum.filter(&String.match?(&1, ~r/^[-0-9|A-Z]+$/))
 
-    {valve, String.to_integer(flow), List.to_tuple(tunnels)}
+    Map.put(acc, valve, {String.to_integer(flow), tunnels})
   end)
-  |> IO.inspect()
 
 defmodule Foo do
-  def solve1(input_data, target_row) do
+  def solve1(input_data) do
+    explore("AA", input_data, 0, [], %{}, 0)
+  end
+
+  defp explore(_, _input_data, score, _opened, cache, 30) do
+    {score, cache}
+  end
+
+  defp explore(current, input_data, score, opened, cache, minute) do
+    cache_value = Map.get(cache, {current, opened, minute})
+
+    if cache_value != nil do
+      {cache_value, cache}
+    else
+      {flow, tunnels} = Map.get(input_data, current)
+      max_score = 0
+
+      {max_score, cache} =
+        if flow > 0 and !Enum.member?(opened, current) do
+          opened = opened ++ [current]
+          score = score + flow * (30 - minute - 1)
+          {new_score, cache} = explore(current, input_data, score, opened, cache, minute + 1)
+          {max(new_score, max_score), cache}
+        else
+          {max_score, cache}
+        end
+
+      {max_score, cache} =
+        tunnels
+        |> Enum.reduce({max_score, cache}, fn x, {max_score, cache} ->
+          {new_score, cache} = explore(x, input_data, score, opened, cache, minute + 1)
+          {max(new_score, max_score), cache}
+        end)
+
+      cache = Map.put(cache, {current, opened, minute}, max_score)
+      {score, cache}
+    end
   end
 end
 
-# IO.puts("Answer to part 1 = #{result}")
+{_, cache} = Foo.solve1(input_data)
+result = Map.values(cache) |> Enum.reduce(0, &max/2)
+
+IO.puts("Answer to part 1 = #{result}")
