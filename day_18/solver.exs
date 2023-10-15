ffc1684d03d20aa1e23df4b2f05ec3b863177d13
Matt Clarke <matt.clarke@ess.eu>
2023-10-15 14:31:54 +0200

    18: part 2 done

--- day_18/solver.exs
+++ day_18/solver.exs
@@ -37,12 +37,64 @@ defmodule Foo do
   end
 
   def solve2(input_data) do
+    {max_x, max_y, max_z} =
+      input_data
+      |> Enum.reduce({-1, -1, -1}, fn {x, y, z}, {mx, my, mz} ->
+        {max(x, mx), max(y, my), max(z, mz)}
+      end)
+
+    water = find_water(input_data, MapSet.new(), {max_x + 1, max_y + 1, max_z + 1}, {-1, -1, -1})
+
+    input_data
+    |> Enum.reduce(0, fn coord, acc ->
+      @directions
+      |> Enum.reduce(acc, fn dir, acc ->
+        new_coord =
+          {elem(coord, 0) + elem(dir, 0), elem(coord, 1) + elem(dir, 1),
+           elem(coord, 2) + elem(dir, 2)}
+
+        if MapSet.member?(water, new_coord) do
+          acc + 1
+        else
+          acc
+        end
+      end)
+    end)
+  end
+
+  defp find_water(input_data, water, {max_x, max_y, max_z} = maxs, {x, y, z}) do
+    water = MapSet.put(water, {x, y, z})
+
+    @directions
+    |> Enum.reduce(water, fn {dx, dy, dz}, water ->
+      coord = {x + dx, y + dy, z + dz}
+
+      cond do
+        MapSet.member?(input_data, coord) ->
+          water
+
+        MapSet.member?(water, coord) ->
+          water
+
+        x + dx < -1 or x + dx > max_x ->
+          water
+
+        y + dy < -1 or y + dy > max_y ->
+          water
+
+        z + dz < -1 or z + dz > max_z ->
+          water
+
+        true ->
+          find_water(input_data, water, maxs, coord)
+      end
+    end)
   end
 end
 
 result = Foo.solve1(input_data)
 IO.puts("Answer to part 1 = #{result}")
 
-# result = Foo.solve2(input_data)
+result = Foo.solve2(input_data)
 
-# IO.puts("Answer to part 2 = #{result}")
+IO.puts("Answer to part 2 = #{result}")
